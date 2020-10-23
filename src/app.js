894f72e05dd30d0c40923a062928003915c02721
andreluiz-web <andreluizdefreitas0@gmail.com>
2020-10-22 23:38:13 -0300

    save files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -15,7 +14,19 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(newRepository);
+
+  return response.json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
